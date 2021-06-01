28a0e66507e52a6b93f320ffff15629716ce5b13
Marko Tasic <mtasic85@gmail.com>
2021-06-01 22:59:34 +0200

    union

--- union.h
+++ union.h
@@ -1,7 +1,15 @@
 #ifndef CO_UNION_H
 #define CO_UNION_H
 
+struct co_union_t;
+
 #include <stdlib.h>
 #include "object.h"
+
+typedef struct co_union_t {
+    struct co_object_t *type;       // type
+    struct co_object_t *name;       // str
+    struct co_object_t *bases;      // list[type]
+} co_union_t;
 
 #endif
