e584ee146481bf7fd7c5d0968c8cc2c1f91a3152
Marko Tasic <mtasic85@gmail.com>
2021-06-02 21:01:14 +0200

    main commented

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 #include "builtins.h"
 
 int main(int argc, char **argv) {
+    // root context
     co_ctx_t *root_ctx = co_ctx_new(NULL);
     co_object_t *res;
 
+    // root context - builtins
     res = co_builtins_init(root_ctx);
     
+    // context with root contect as parent
     co_ctx_t *ctx = co_ctx_new(root_ctx);
     co_ctx_free(ctx);
 
+    // cleanup
     CO_OBJECT_DEC_RC(root_ctx, res);
     return 0;
 }
