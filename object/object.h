dd85a54ee5980276b6226ac6130cb7fffe1799e9
Marko Tasic <mtasic85@gmail.com>
2021-06-02 23:06:21 +0200

    object/type

--- object/object.h
+++ object/object.h
@@ -1,13 +1,13 @@
 #ifndef CO_OBJECT_H
 #define CO_OBJECT_H
 
-enum co_kind_t;
 union co_value_t;
 struct co_object_t;
 
 #include <stdlib.h>
 #include <stdint.h>
 #include "../core/ctx.h"
+#include "./kind.h"
 #include "./bool.h"
 #include "./bytes.h"
 #include "./code.h"
@@ -41,40 +41,6 @@ struct co_object_t;
         co_object_free(ctx, (struct co_object_t *)(obj)); \
     } \
 } while(0)
-
-typedef enum co_kind_t {
-    CO_KIND_TYPE,
-    CO_KIND_BOOL,
-    CO_KIND_U8,
-    CO_KIND_U16,
-    CO_KIND_U32,
-    CO_KIND_U64,
-    CO_KIND_I8,
-    CO_KIND_I16,
-    CO_KIND_I32,
-    CO_KIND_I64,
-    CO_KIND_F32,
-    CO_KIND_F64,
-    CO_KIND_BYTES,
-    CO_KIND_STR,
-    CO_KIND_LIST,
-    CO_KIND_MUT_LIST,
-    CO_KIND_DICT,
-    CO_KIND_MUT_DICT,
-    CO_KIND_NS,
-    CO_KIND_CODE,
-    CO_KIND_FUNC,
-    CO_KIND_CLOSURE, // ?
-    CO_KIND_TUPLE,
-    CO_KIND_UNION,
-    CO_KIND_OPTION,
-    CO_KIND_SOME,
-    CO_KIND_NONE,
-    CO_KIND_RESULT,
-    CO_KIND_OK,
-    CO_KIND_ERR,
-    CO_KIND_PTR
-} co_kind_t;
 
 typedef union co_value_t {
     struct co_type_t *type;
