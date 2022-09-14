f1b2544197f53f3c3d35643c4a24989f62ff0a33
Marko Tasic <mtasic85@gmail.com>
2022-09-14 22:00:55 +0200

    ctx

--- colony.c
+++ colony.c
@@ -3,27 +3,27 @@
 /*
  * ctx
  */
-co_object_t co_ctx_new(co_object_t obj, co_object_t args, co_object_t kwargs) {
+co_object_t co_ctx_new(co_object_t ctx_cls, co_object_t obj, co_object_t args, co_object_t kwargs) {
     co_object_t self;
-    co_ctx_t *ctx;
+    co_ctx_t *ctx = NULL;
 
     ctx = calloc(1, sizeof(co_ctx_t));
     ctx->rc = 1;
 
     self = (co_object_t){
         .k = CO_KIND_CTX,
-        .v = ctx,
+        .v = (co_gc_t*)ctx,
     };
 
     return self;
 }
 
-co_object_t co_ctx_free(co_object_t obj, co_object_t args, co_object_t kwargs) {
+co_object_t co_ctx_free(co_object_t ctx, co_object_t obj, co_object_t args, co_object_t kwargs) {
     co_object_t self;
     return self;
 }
 
-co_object_t co_ctx_spawn(co_object_t ctx, co_object_t args, co_object_t kwargs) {
+co_object_t co_ctx_spawn(co_object_t ctx, co_object_t obj, co_object_t args, co_object_t kwargs) {
     co_object_t self;
     return self;
 }
