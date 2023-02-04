c3b22a342a43574d14a1d7ec61ac120394cbf53c
Marko Tasic <mtasic85@gmail.com>
2023-02-04 20:39:50 +0100

    bit and bitmask operations on C uints

--- colony.h
+++ colony.h
@@ -6,6 +6,17 @@
 #include <stdbool.h>
 #include <uchar.h>
 
+#define BIT_SET(a, b) ((a) |= (1ULL << (b)))
+#define BIT_CLEAR(a, b) ((a) &= ~(1ULL << (b)))
+#define BIT_FLIP(a, b) ((a) ^= (1ULL << (b)))
+#define BIT_CHECK(a, b) (!!((a) & (1ULL << (b))))
+
+#define BITMASK_SET(x, mask) ((x) |= (mask))
+#define BITMASK_CLEAR(x, mask) ((x) &= (~(mask)))
+#define BITMASK_FLIP(x, mask) ((x) ^= (mask))
+#define BITMASK_CHECK_ALL(x, mask) (!(~(x) & (mask)))
+#define BITMASK_CHECK_ANY(x, mask) ((x) & (mask))
+
 enum co_kind_t;
 union co_value_t;
 struct co_obj_t;
